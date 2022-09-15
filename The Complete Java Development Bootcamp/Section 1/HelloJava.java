c053077cf2ebbb20c843e97f381df165e4ee9d76
unknown <erskine.duenas@gmail.com>
2022-09-15 12:32:18 +0800

    update

--- The Complete Java Development Bootcamp/Section 1/HelloJava.java
+++ The Complete Java Development Bootcamp/Section 1/HelloJava.java
@@ -4,6 +4,11 @@ public class HelloJava {     //1. The class keyword = class ; 2. The class name 
     }
     
 }
+//How I wrote this code? Step by step:
+//public class boiler plate   line of code #1
+//main boiler plate           line of code #2 + run | Debug
+//System.out.println boiler plate    line of code #3
+//then I wrote the Hello Java        line of code #3
 
 // How to compile and run JAVA code:
 // Step 1: write javac .\<JavaFileName>.java in terminal, this is to compile the java file
