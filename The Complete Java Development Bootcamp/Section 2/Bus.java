dac1bf3c75ae2de432cf0f257e3a12780e067c2a
unknown <erskine.duenas@gmail.com>
2022-09-15 19:05:23 +0800

    update

--- The Complete Java Development Bootcamp/Section 2/Bus.java
+++ The Complete Java Development Bootcamp/Section 2/Bus.java
@@ -1,11 +1,14 @@
 public class Bus {
     public static void main(String[] args) {
+        String sentence = "Number of passengers: ";
         int passengers = 0;
         passengers = passengers + 9;      // you can also use passenger += 9;
-        System.out.println(passengers);
+        System.out.println(sentence + passengers);
         passengers = passengers - 5;      // you can also use passenger -= 5;
-        System.out.println(passengers);
+        System.out.println(sentence + passengers);
         passengers -= 4;                  // <---- like this
-        System.out.println(passengers);
+        System.out.println(sentence + passengers);
+        sentence = "end of shift";
+        System.out.println(sentence);
     }
 }
---
+++ The Complete Java Development Bootcamp/Section 2/Form.java
@@ -0,0 +1,5 @@
+public class Form {
+    public static void main(String[] args) {
+        
+    }
+}
