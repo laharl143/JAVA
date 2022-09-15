0c732b6f785e280bf3733d776a290772b0b8422c
unknown <erskine.duenas@gmail.com>
2022-09-15 19:14:19 +0800

    update

--- The Complete Java Development Bootcamp/Section 2/Form.java
+++ The Complete Java Development Bootcamp/Section 2/Form.java
@@ -1,5 +1,16 @@
 public class Form {
     public static void main(String[] args) {
-        
+        char gender = 'F';                      //Char is used for single Letters ; it is also important to use single quotation marks for char. if you use double quotation mark, it will lead to error.
+        System.out.println("Gender: " + gender);
     }
 }
+
+
+// FAQS:
+// Why use char?
+// Memory: char consumes less memory
+// Performance: char is faster than string
+
+// Rule of thumb:
+// Use char to store single characters
+// Use String to store text
--- The Complete Java Development Bootcamp/Section 2/JoiningStrings.java
+++ The Complete Java Development Bootcamp/Section 2/JoiningStrings.java
@@ -1,6 +1,6 @@
 public class JoiningStrings {
     public static void main(String[] args) {
-        String sentence = "His name is: ";
+        String sentence = "His name is: ";          //String should always be uppercase
         String placeholder = "Harry";
         System.out.println(sentence + placeholder);
 
