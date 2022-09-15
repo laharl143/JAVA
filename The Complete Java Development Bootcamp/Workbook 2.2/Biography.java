fbb1c353318c3eefeac67ad90a42e2979a41698e
unknown <erskine.duenas@gmail.com>
2022-09-15 19:35:16 +0800

    update

---
+++ The Complete Java Development Bootcamp/Workbook 2.2/Biography.java
@@ -0,0 +1,19 @@
+public class Biography {
+    public static void main(String[] args) {
+
+        String name = "Erskine";
+        int age = 26;
+        String country = "Philippines";//make a country variable
+        String sport = "Football";//make a sport variable
+        int hours = 6;//make an hours variable
+        String game = "Dota";//make a game variable
+        String subject = "Math";//make a subject variable
+        char grade = 'A';//make a grade variable
+     
+        System.out.println("My name is " + name + ". I'm " + age + " years old, and I'm from " + country + ".");
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hours a day" + ".");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + ", I scored an " + grade + ".");
+
+    }
+}
