18dbb76000139e2215732d0e8c604f4153d2ecf9
unknown <erskine.duenas@gmail.com>
2022-09-15 12:27:20 +0800

    update

---
+++ The Complete Java Development Bootcamp/Section 1/HelloJava.java
@@ -0,0 +1,10 @@
+public class HelloJava {     //1. The class keyword = class ; 2. The class name = HelloJava 
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+    
+}
+
+// How to compile and run JAVA code:
+// Step 1: write javac .\<JavaFileName>.java in terminal, this is to compile the java file
+// Step 2: write java <JavaFileName>.class in terminal to run the compiled file.
