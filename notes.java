e5a15f330b871a17c28b56987e0eb129678fb3b9
unknown <erskine.duenas@gmail.com>
2022-09-22 18:07:06 +0800

    udpate

---
+++ notes.java
@@ -0,0 +1,35 @@
+/*
+//How I wrote this code? Step by step:
+//public class boiler plate   line of code #1
+//main() boiler plate           line of code #2 + run | Debug
+//System.out.println boiler plate    line of code #3
+//then I wrote the Hello Java        line of code #3
+
+// How to compile and run JAVA code:
+// Step 1: write "javac .\<JavaFileName>.java" in terminal, this is to compile the java file
+// Step 2: write "java <JavaFileName>".class in terminal to run the compiled file.
+
+// FAQS:
+// Why use char?
+// Memory: char consumes less memory
+// Performance: char is faster than string
+
+// Rule of thumb:
+// Use char to store single characters
+// Use String to store text
+
+// Notes: 
+// int variables can only store from -2,147,483,648 to +2,147,483,648  ; only in the 2billion range
+//      Amount of memory: 4bytes 
+
+// long variables can only store from -9,223,372,036,854,775,807 to +9,223,372,036,854,775,807 ; up to 9 quintillion range
+//      Amount of memory: 8bytes 
+
+
+// Why not always use long?
+// long takes 8bytes of memory compared to int which is only 4bytes ; hence it is faster
+
+
+
+
+ */
