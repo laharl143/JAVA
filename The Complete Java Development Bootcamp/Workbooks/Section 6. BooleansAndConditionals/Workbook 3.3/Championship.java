19f2c4b4b97052ee0f1bd5712f00ce039d923877
unknown <erskine.duenas@gmail.com>
2022-09-29 12:21:56 +0800

    update

--- The Complete Java Development Bootcamp/Workbooks/Section 6. BooleansAndConditionals/Workbook 3.3/Championship.java
+++ The Complete Java Development Bootcamp/Workbooks/Section 6. BooleansAndConditionals/Workbook 3.3/Championship.java
@@ -1,14 +1,24 @@
 public class Championship {
     public static void main(String[] args) {
 
-        int gryffindor = 400;    //gryffindor points
-        int ravenclaw = 200;    //ravenclaw points
+        int gryffindor = 100;    //gryffindor points
+        int ravenclaw = 500;    //ravenclaw points
 
         // int margin = amount of points by which gryffindor scored over ravenclaw;
+        int margin = (gryffindor - ravenclaw);
 
-
+        if (margin >= 300) {
+            System.out.println("Gryffindor takes the house cup!");
+        } else if (margin >= 0) {
+            System.out.println("In second place, Gryffindor!");
+        } else if (margin >= -100) {
+            System.out.println("In third place, Gryffindor");
+        } else {
+            System.out.println("In fourth place, Gryffindor");
+        }
         //if gryffindor wins by a margin of 300 points:
              //print: Gryffindor takes the house cup!
+
 
         //if gryffindor wins by a margin of any points: 
             //print: In second place, Gryffindor!
@@ -21,3 +31,20 @@ public class Championship {
         
     }
 }
+/*
+final output:
+        int gryffindor = 850;
+        int ravenclaw = 500;
+>>Gryffindor takes the house cup!
+        int gryffindor = 620;
+        int ravenclaw = 500;
+>>In second place, Gryffindor!
+        int gryffindor = 450;
+        int ravenclaw = 500;
+>>In third place, Gryffindor!
+        int gryffindor = 100;
+        int ravenclaw = 500;
+>>In fourth place, Gryffindor!
+
+
+ */
