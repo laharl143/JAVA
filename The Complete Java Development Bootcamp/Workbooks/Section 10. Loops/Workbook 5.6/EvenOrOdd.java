4282d4ec85135001803608eb707588742e247ee4
unknown <erskine.duenas@gmail.com>
2022-10-10 12:18:02 +0800

    update

--- The Complete Java Development Bootcamp/Workbooks/Section 10. Loops/Workbook 5.6/EvenOrOdd.java
+++ The Complete Java Development Bootcamp/Workbooks/Section 10. Loops/Workbook 5.6/EvenOrOdd.java
@@ -2,7 +2,13 @@ public class EvenOrOdd {
     public static void main(String[] args) {
         // Task 1 – Make a for loop that counts from 0 to 19. 
         
-        
+        for (int i = 0; i <= 19; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " - even");
+             } else {
+                System.out.println(i + " - odd");
+             }
+        }
         
         /* Task 2
              
@@ -10,6 +16,6 @@ public class EvenOrOdd {
             2. If the number is odd, print ' – odd' beside the number.
 
          */
-
+         
     }
 }
