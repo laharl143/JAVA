269fca92116d71faed86b64a08727bf9f9ba845d
unknown <erskine.duenas@gmail.com>
2022-10-10 12:09:04 +0800

    update

--- The Complete Java Development Bootcamp/Workbooks/Section 10. Loops/Workbook 5.5/CounterTool.java
+++ The Complete Java Development Bootcamp/Workbooks/Section 10. Loops/Workbook 5.5/CounterTool.java
@@ -5,7 +5,7 @@ public class CounterTool {
         Scanner scan = new Scanner(System.in);
         System.out.println("I hear you like to count by threes\n");
         System.out.println("Jimmy: It depends.");
-        System.out.println("Oh, ok...");
+        System.out.println("Oh, ok...\n");
 
       /* Task 1 – Get integers from the user.
 
@@ -20,14 +20,37 @@ public class CounterTool {
                 pick up the user's answer 
                 
         */
-        
+        System.out.print(" 1. Pick a number to count by: ");
+        int numberStep = scan.nextInt();
+
+        System.out.print(" 2. Pick a number to start counting from: ");
+        int numberStart = scan.nextInt();
+
+        System.out.print(" 3. Pick a number to count to: ");
+        int numberStop = scan.nextInt();
 
       /* Task 2 – Create a for loop that:
             1. starts from the second number.
             2. stops at the third number. 
             3. counts in steps of the first.
       */
+        for (int i = numberStart; i <= numberStop; i += numberStep) {
+            System.out.print(i + " ");
+        }
 
         scan.close();
     }
 }
+
+/*
+final output:
+I hear you like to count by threes
+
+Jimmy: It depends.
+Oh, ok...
+
+ 1. Pick a number to count by: 3
+ 2. Pick a number to start counting from: 6
+ 3. Pick a number to count to: 66
+6 9 12 15 18 21 24 27 30 33 36 39 42 45 48 51 54 57 60 63 66 
+ */
--- The Complete Java Development Bootcamp/Workbooks/Section 10. Loops/Workbook 5.6/EvenOrOdd.java
+++ The Complete Java Development Bootcamp/Workbooks/Section 10. Loops/Workbook 5.6/EvenOrOdd.java
@@ -2,6 +2,8 @@ public class EvenOrOdd {
     public static void main(String[] args) {
         // Task 1 – Make a for loop that counts from 0 to 19. 
         
+        
+        
         /* Task 2
              
             1. If the number is even, print ' – even' beside the number.
