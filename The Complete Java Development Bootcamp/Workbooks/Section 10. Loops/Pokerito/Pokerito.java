49373030433217dda8fb9840ff3c6e776b33a89a
unknown <erskine.duenas@gmail.com>
2022-10-19 11:49:30 +0800

    update

--- The Complete Java Development Bootcamp/Workbooks/Section 10. Loops/Pokerito/Pokerito.java
+++ The Complete Java Development Bootcamp/Workbooks/Section 10. Loops/Pokerito/Pokerito.java
@@ -4,6 +4,19 @@ public class Pokerito {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+
+        System.out.println("Let's play Pokerito. Press <enter> when you're ready.");
+        scan.nextLine();
+
+        System.out.println("It's like Poker, but a lot simpler.\n");
+        System.out.println("- There are two players, you and the computer.");
+        System.out.println("- The dealer will give each player one card.");
+        System.out.println("- Then, the dealer will draw five cards (the river)");
+        System.out.println("- The player with the most river matches wins!");
+        System.out.println("- If the matches are equal, everyone's a winner!\n");
+        
+        System.out.println("- Ready? Press <enter> if you are.");
+        scan.nextLine();
         /*Task 2: Explain the rules
 
         >>Let's play Pokerito. Type anything when you're ready.
@@ -19,8 +32,12 @@ public class Pokerito {
         >> • Ready? Type anything if you are.
         |
         */
-        
-
+        String yourCard = randomCard();
+        String computerCard = randomCard();
+        System.out.println("Here's your card:" );
+        System.out.println(yourCard);
+        System.out.println("Here's the computer's card:" );
+        System.out.println(computerCard);
         /*Task 3: Present the user with a card
          println 'Here's your card:'
          <show card>
@@ -32,6 +49,21 @@ public class Pokerito {
         int yourMatches = 0;
         int computerMatches =0;
 
+
+        System.out.println("Now, the dealer will draw five cards. Press <enter> to draw each card.");
+        for (int i = 1; i <= 5; i++) {
+            scan.nextLine();
+            String draw = randomCard();
+
+            if (yourCard.equals(draw)){
+                yourMatches++;
+            }
+            if (computerCard.equals(draw)){
+                computerMatches++;
+            }
+            System.out.println("Card" + i + "\n");
+            System.out.println(draw);
+        }
         /** Task 4 - Draw five cards
          * 
          * • print:  Now, the dealer will draw five cards. Press enter to continue.
@@ -46,6 +78,16 @@ public class Pokerito {
          *      ...
          */
 
+        System.out.println("Your number of matches: " + yourMatches);
+        System.out.println("Computer number of matches: " + computerMatches);
+
+        if (yourMatches > computerMatches) {
+            System.out.println("You win!");
+        } else if (yourMatches < computerMatches) {
+            System.out.println("The computer wins!");
+        } else if (yourMatches == computerMatches) {
+            System.out.println("Everyone wins!");
+        }
         /** Task 5 - Get the winner
          * 
          * • Count your number of matches.
@@ -61,6 +103,116 @@ public class Pokerito {
          scan.close();
     }
 
+
+    public static String randomCard() {
+        double randomNumber = Math.random() * 13;
+        randomNumber += 1;
+        switch ((int)randomNumber) {
+            case 1:   
+            return "   _____\n"+
+                   "  |A _  |\n"+ 
+                   "  | ( ) |\n"+
+                   "  |(_'_)|\n"+
+                   "  |  |  |\n"+
+                   "  |____V|\n";
+            case 2:
+            return  "   _____\n"+              
+                    "  |2    |\n"+ 
+                    "  |  o  |\n"+
+                    "  |     |\n"+
+                    "  |  o  |\n"+
+                    "  |____Z|\n";
+                  
+            case 3:
+            return"   _____\n" +
+                  "  |3    |\n"+
+                  "  | o o |\n"+
+                  "  |     |\n"+
+                  "  |  o  |\n"+
+                  "  |____E|\n";
+
+            case 4:
+            return "   _____\n" +
+                   "  |4    |\n"+
+                   "  | o o |\n"+
+                   "  |     |\n"+
+                   "  | o o |\n"+
+                   "  |____h|\n";
+
+            case 5:
+            return  "   _____ \n" +
+                    "  |5    |\n" +
+                    "  | o o |\n" +
+                    "  |  o  |\n" +
+                    "  | o o |\n" +
+                    "  |____S|\n";
+
+            case 6:
+            return  "   _____ \n" +
+                    "  |6    |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  | o o |\n" +
+                    "  |____6|\n";
+
+            case 7:
+            return  
+                    "   _____ \n" +
+                    "  |7    |\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |____7|\n";
+          
+
+            case 8:
+            return  "   _____ \n" +
+                    "  |8    |\n" +
+                    "  |o o o|\n" +
+                    "  | o o |\n" +
+                    "  |o o o|\n" +
+                    "  |____8|\n";
+            case 9:
+            return  "   _____ \n" +
+                    "  |9    |\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |____9|\n";
+
+            case 10:
+            return  "   _____ \n" +
+                    "  |10  o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |o o o|\n" +
+                    "  |___10|\n";
+            case 11:
+            return  "   _____\n" +
+                    "  |J  ww|\n"+ 
+                    "  | o {)|\n"+ 
+                    "  |o o% |\n"+ 
+                    "  | | % |\n"+ 
+                    "  |__%%[|\n";
+            case 12:
+            return  "   _____\n" +
+                    "  |Q  ww|\n"+ 
+                    "  | o {(|\n"+ 
+                    "  |o o%%|\n"+ 
+                    "  | |%%%|\n"+ 
+                    "  |_%%%O|\n";
+            case 13:
+            return  "   _____\n" +
+                    "  |K  WW|\n"+ 
+                    "  | o {)|\n"+ 
+                    "  |o o%%|\n"+ 
+                    "  | |%%%|\n"+ 
+                    "  |_%%%>|\n";
+            default: 
+            return "This shouldn't get called.";
+        }
+    }
+
     /** Task 1
      * 
      * Function name – randomCard
@@ -72,3 +224,89 @@ public class Pokerito {
      */
 
 }
+
+/*
+final output:
+Let's play Pokerito. Press <enter> when you're ready.
+
+It's like Poker, but a lot simpler.
+
+- There are two players, you and the computer.
+- The dealer will give each player one card.
+- Then, the dealer will draw five cards (the river)
+- The player with the most river matches wins!
+- If the matches are equal, everyone's a winner!
+
+- Ready? Press <enter> if you are.
+
+Here's your card:
+   _____
+  |3    |
+  | o o |
+  |     |
+  |  o  |
+  |____E|
+
+Here's the computer's card:
+   _____
+  |2    |
+  |  o  |
+  |     |
+  |  o  |
+  |____Z|
+
+Now, the dealer will draw five cards. Press <enter> to draw each card.
+
+Card1
+
+   _____
+  |7    |
+  | o o |
+  |o o o|
+  | o o |
+  |____7|
+
+
+Card2
+
+   _____
+  |7    |
+  | o o |
+  |o o o|
+  | o o |
+  |____7|
+
+
+Card3
+
+   _____
+  |9    |
+  |o o o|
+  |o o o|
+  |o o o|
+  |____9|
+
+
+Card4
+
+   _____
+  |4    |
+  | o o |
+  |     |
+  | o o |
+  |____h|
+
+
+Card5
+
+   _____
+  |2    |
+  |  o  |
+  |     |
+  |  o  |
+  |____Z|
+
+Your number of matches: 0
+Computer number of matches: 1
+The computer wins!
+ */
